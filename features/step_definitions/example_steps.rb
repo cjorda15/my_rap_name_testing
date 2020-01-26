baada54201a2663ac80eefc5932311f0a4e4888b
Chris Jordan <crobertjordan@yahoo.com>
2020-01-26 09:49:22 -0700

    Testing complete, though not covering the checkbox for use nickname, not sure what it actually does. Need to refactor with less brittle selectors that should also be variables instead of given string selectors, probably should rearrange the test approach to be more step like, but still want to keep them independent

--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -1,7 +1,48 @@
-Given(/^I am on the homepage$/) do
-  find('.logo')
+$firstname='Ken'
+$lastinitial='Z'
+
+When(/^I am on the homepage$/) do
+  expect(page.current_path).to eq('/')
 end
 
-Then(/^I see the text "(.*?)"$/) do |text|
-  page.has_text?(text)
+And(/^I submit as a ([^"]*)$/) do | gender|
+  gender[0] = gender[0].upcase
+  find('[name="'+ gender +'"]').click
+end
+
+And(/^I submit ([^"]*) times$/) do | count |
+  count = count.to_i
+  until count.to_i <= 0
+    count = count-1
+    find('[name="Female"]').click
+  end
+end
+
+And(/^I provide input for all the fields$/) do
+  fill_in 'firstname', with: $firstname
+  fill_in 'lastinitial', with: $lastinitial
+end
+
+
+Then(/^I see the input fields$/) do
+  find('[name="firstname"]')
+  find('[name="lastinitial"]')
+end
+
+Then(/^I see the user error shown$/) do
+  expect(page).to have_text("You must enter your first name.")
+end
+
+Then(/^I see input for all the fields$/) do
+  expect(page).to have_field('firstname', with: $firstname)
+  expect(page).to have_field('lastinitial', with: $lastinitial)
+end
+
+Then(/^I see a rap name$/) do
+  expect(page).to have_selector('td:nth-child(1) > div > table > tbody > tr',
+   count: 1)
+end
+
+Then(/^I see exactly ([^"]*) rap names$/) do |amount|
+  expect(page).to have_selector('td:nth-child(1) > div > table > tbody > tr', count: amount.to_i)
 end
